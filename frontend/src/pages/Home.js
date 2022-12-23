122fb44ae3467ac2a8d25dea1827d1c313111f15
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-12-23 17:30:20 -0500

    Create a seperate components and pages in react

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div class="container my-5">
+         <h1>FitnessJourney</h1>
+       </div>
+    )
+}
+
+export default Home;
