5918bb5c762fabc243e7bcd1b73ca67092b99390
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-12-28 00:03:31 -0500

    Create Post Serializer

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,1 +1,11 @@
-# Serializers
+from rest_framework import serializers
+from ..models import (
+    Post
+)
+from base.api.serializers import UserSerializer
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ('user', 'image', 'day', 'description', 'date', 'datetime')
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import PostSerializer
+from ..models import Post
 
 @api_view(['GET'])
 def ALLPOSTSVIEW(request, *args, **kwargs):
-    return Response([], status=200)
+    qs = Post.objects.all()
+    serializer = PostSerializer(qs, many=True)
+    data = serializer.data
+    return Response(data, status=200)
