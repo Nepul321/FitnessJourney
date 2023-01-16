ebb7213824d3fedb7560e314624c969a318b4865
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-01-16 16:04:04 -0500

    Update urls

--- users/api/urls.py
+++ users/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     UsersView,
     UserLoginView,
-    UserAccountView
+    UserAccountView,
+    UserChangePasswordView
 )
 
 urlpatterns = [
     path('', UsersView, name="users"),
     path('login/', UserLoginView, name="users-login"),
     path('user/', UserAccountView, name="users-view"),
+    path('password/', UserChangePasswordView, name="users-password"),
 ]
