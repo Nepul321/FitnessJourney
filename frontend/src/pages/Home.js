0bfd931ba7a529ce8db717926579ffe093eea5d1
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-01-28 15:07:27 -0500

    Create a navbar component in react

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Home() {
     return (
+        <div className="home">
+        <Navbar />
         <div class="container my-5">
          <h1>FitnessJourney</h1>
+         <hr />
+         <p style={{fontSize : "25px"}}>Newest Posts...</p>
        </div>
+        </div>
     )
 }
 
