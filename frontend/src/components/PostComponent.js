dae6aaa5062d98ef4d4c60c458e3d7b79e929bea
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-01-28 15:40:58 -0500

    Make post componenet look better

--- frontend/src/components/PostComponent.js
+++ frontend/src/components/PostComponent.js
@@ -3,13 +3,27 @@ import React from "react";
 function Post(props) {
    const {post} = props;
 
-   console.log(post)
-
    return (
     <div className="post mb-4">
-        <p>Day : {post.day}</p>
-        <p>Description : {post.description}</p>
-        <p>Date : {post.date}</p>
+        <div className="card mb-3">
+    <div className="row g-0">
+      <div className="col-md-4">
+        <img src={post.image} className="img-fluid rounded-start" alt="..." />
+      </div>
+      <div className="col-md-8">
+        <div className="card-body">
+          <h5 className="card-title">Day : {post.day}</h5>
+          <p className="card-text">{post.description}</p>
+          <p className="card-text">{post.date}</p>
+          <p className="card-text">@{post.user.username}</p>
+          {/* <div className="btn-group">
+            <a href="" className="btn btn-outline-primary">View</a>
+            <a href="" className="btn btn-outline-secondary">Edit</a>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  </div>
     </div>
    )
 }
