c4bc72b1087c1df71d0c967ffa27295bd5091d21
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-01-28 15:14:32 -0500

    Create components for individual posts and lists

---
+++ frontend/src/components/PostComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Post(props) {
+   const {post} = props;
+   
+   return (
+    <div className="post">
+        <p>a post</p>
+    </div>
+   )
+}
+
+export default Post;
---
+++ frontend/src/components/Posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PostComponent(props) {
+ const {posts} = props;
+ return (
+    <div className="posts">
+    
+    </div>
+ )
+}
+
+export default PostComponent;
