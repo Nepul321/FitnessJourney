3e52b3b91ff17b0e03a4b26e719321495f505d83
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-01-30 22:13:04 -0500

    Use react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/about" exact element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about container my-5 ">
+      <h1>About</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut
+        tincidunt est. Duis vel erat id ligula dictum fringilla in ac urna. Cras
+        malesuada sagittis eros nec interdum. Nam sit amet ligula tellus. Orci
+        varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Vivamus quis ante porta, facilisis tortor at, tempus
+        velit. Morbi interdum dolor nec ex pretium, a tempus dui viverra. Nullam
+        diam turpis, mollis in orci eget, euismod ultricies sem. Integer congue
+        commodo sodales. Nunc vitae luctus nulla. Etiam bibendum enim fringilla
+        dapibus gravida.
+      </p>
+    </div>
+  );
+}
+
+export default About;
