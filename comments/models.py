de6928eff6f9a54634103c83a11fbec13103b59a
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-04 16:32:07 -0500

    Create comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE)
+
+    @property
+    def is_child(self):
+        if self.parent is not None:
+            return True
+        else:
+            return False
+
+    def get_children(self):
+        if self.is_child:
+            return None
+        else:
+            return Comment.objects.filter(parent=self)
