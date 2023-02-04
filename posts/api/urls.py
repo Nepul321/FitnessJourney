40511cb84f6e3768a44b027947497febfa7c449b
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-04 15:44:07 -0500

    Post detail view - API

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-    NEWESTPOSTSVIEW
+    NEWESTPOSTSVIEW,
+    PostView
 )
 
 urlpatterns = [
-    path('', NEWESTPOSTSVIEW, name="all-posts")
+    path('', NEWESTPOSTSVIEW, name="all-posts"),
+    path('<int:id>/', PostView, name="individual-post")
 ]
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,8 +1,16 @@
+import django
+django.setup()
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import PostSerializer
+from django.contrib.auth import get_user_model
 from ..models import Post
 from django.db.models import Q
+import jwt
+
+User = get_user_model()
+
 
 @api_view(['GET'])
 def NEWESTPOSTSVIEW(request, *args, **kwargs):
@@ -11,4 +19,34 @@ def NEWESTPOSTSVIEW(request, *args, **kwargs):
     )
     serializer = PostSerializer(qs, many=True)
     data = serializer.data
+    return Response(data, status=200)
+
+@api_view(['GET'])
+def PostView(request, id,  *args, **kwargs):
+    qs = Post.objects.filter(id=id)
+    if not qs:
+        return Response({"detail" : "Post not found"}, status=404)
+    obj = qs.first()
+    try:
+        auth = request.headers['Authorization']
+        token = auth.replace("Bearer ", "")
+    except:
+        token = None
+
+    user = None
+
+    try:
+     if token:
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        user = User.objects.filter(id=payload['id']).first()
+    except jwt.ExpiredSignatureError:
+        pass
+
+    if obj.is_private == True:
+        if obj.user != user:
+            return Response({"detail" : "You can't view this post"}, status=403)
+    
+    serializer = PostSerializer(obj)
+    data = serializer.data
+
     return Response(data, status=200)
