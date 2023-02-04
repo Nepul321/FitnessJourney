f6844019efc561f6226a01be43820aa337cc7c37
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-04 16:01:03 -0500

    Add view count to post model

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,14 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class VideoPostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime' , ]
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="media/posts/images/%Y/%m/%d/%H/%M")
@@ -14,6 +22,13 @@ class Post(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="video_post_likes", blank=True)
     dislikes = models.ManyToManyField(User, related_name="video_posts_dislikes", blank=True)
-    
+    views = models.ManyToManyField(VideoPostView, related_name="video_post_views", blank=True)
+    view_count = models.IntegerField(default=0)
+
     class Meta:
         ordering = ['-datetime', ]
+
+    def set_view_count(self):
+        views = self.views.count()
+        self.view_count = views
+        self.save()
