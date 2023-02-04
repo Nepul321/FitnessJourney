b4b362f528849078a62c4419fd3e403d65dc8b47
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-04 17:04:58 -0500

    show comments in post serializer

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -3,15 +3,17 @@ from ..models import (
     Post
 )
 from base.api.serializers import UserPublicSerializer
+from comments.api.serializers import CommentSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     user = UserPublicSerializer(read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
     dislikes = serializers.SerializerMethodField(read_only=True)
+    comments = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ('user', 'image', 'day', 'description', 'date', 'datetime', 'is_private', 'view_count', 'likes', 'dislikes')
+        fields = ('user', 'image', 'day', 'description', 'date', 'datetime', 'is_private', 'view_count', 'likes', 'dislikes', 'comments')
 
     def get_likes(self, obj):
         like_count = obj.likes.count()
@@ -20,5 +22,10 @@ class PostSerializer(serializers.ModelSerializer):
     def get_dislikes(self, obj):
         dislike_count = obj.dislikes.count()
         return dislike_count
+    
+    def get_comments(self, obj):
+        qs = obj.comments.all()
+        serializer = CommentSerializer(qs, many=True)
+        return serializer.data
 
     
