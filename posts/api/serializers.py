1162c7e40c6c92c0d66969a5ce0ad23df2e6228f
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-04 16:13:27 -0500

    Add like and dislike counts to serializer

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -6,6 +6,19 @@ from base.api.serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    likes = serializers.SerializerMethodField(read_only=True)
+    dislikes = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Post
-        fields = ('user', 'image', 'day', 'description', 'date', 'datetime', 'is_private', 'view_count')
+        fields = ('user', 'image', 'day', 'description', 'date', 'datetime', 'is_private', 'view_count', 'likes', 'dislikes')
+
+    def get_likes(self, obj):
+        like_count = obj.likes.count()
+        return like_count
+    
+    def get_dislikes(self, obj):
+        dislike_count = obj.dislikes.count()
+        return dislike_count
+
+    
